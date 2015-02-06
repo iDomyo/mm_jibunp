1bedb87558e03126457ebe1a94c0dd1f83ea3591
iDomyo <idomyo@gmail.com>
2015-02-06 19:25:48 +0900

    change slims

--- config.rb
+++ config.rb
@@ -69,7 +69,7 @@ page "/feed.xml", layout: false
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# activate :livereload
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -100,4 +100,11 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+end
+
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host   = 'jibunp.com'
+  deploy.path   = '/home/core/share/www/com.jibunp/' 
+
 end
